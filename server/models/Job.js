6c319fe8b00f14a5fc6f6f4923e3ac8cd5de7a5e
Gzx <1003181892@qq.com>
2019-08-15 14:51:26 +0900

    aaa

--- server/models/Job.js
+++ server/models/Job.js
@@ -6,30 +6,6 @@ const schema = new mongoose.Schema({
     },
     jobType: {
         type: String
-    },
-    lastBuild: {
-        type: Number
-    },
-    totalCount: {
-        type: Number
-    },
-    failCount: {
-        type: Number
-    },
-    successCount: {
-        type: Number
-    },
-    skipCount: {
-        type: Number
-    },
-    timestamp: {
-        type: Number
-    },
-    result: {
-        type: String
-    },
-    successRatio:{
-        type: String
     }
 });
 
--- server/routes/job.js
+++ server/routes/job.js
@@ -6,7 +6,7 @@ const JobType = require('../models/JobType');
 
 //mock
 const jobinfo = require('../data/jobInfo');
-const buildinfo = require('../data/build')
+const buildinfo = require('../data/build');
 
 /* GET dashboard Data. */
 router.get('/', async (req, res) => {
@@ -19,13 +19,26 @@ router.get('/', async (req, res) => {
             jobList: [],
         };
         temp.jobType = item.typeName;
-        let today = new Date();
-        let yesterday = new Date(Date.now() - 86400000);
-        temp.jobList = await Job.find({ jobType: item.typeName, timestamp: { $gte: yesterday, $lt: today } });
-        for (let jobItem of temp.jobList) {
-            if (jobItem.result !== 'SUCCESS') {
+        let jobList = await Job.find({ jobType: item.typeName });
+        for (let jobItem of jobList) {
+            let job = {};
+            job.jobName = jobItem.jobName;
+            job.jobType = jobItem.jobType;
+            job.lastBuild = jobinfo.lastBuild.number;
+            let testResult = buildinfo.actions.filter((item) => {
+                return item._class === 'hudson.tasks.junit.TestResultAction'
+            });
+            job.failCount = testResult[0].failCount;
+            job.skipCount = testResult[0].skipCount;
+            job.totalCount = testResult[0].totalCount;
+            job.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
+            job.successRatio = job.successCount / job.totalCount * 100 + '%';
+            job.timestamp = buildinfo.timestamp;
+            job.result = buildinfo.result;
+            if (job.result !== 'SUCCESS') {
                 temp.status = 'FAIL'
             }
+            temp.jobList.push(job)
         }
         data.push(temp);
     }
@@ -36,33 +49,32 @@ router.get('/', async (req, res) => {
    save data to job models
 */
 
-const dailySaveToJobModel = () => {
-    schedule.scheduleJob('30 1 1 * * *', async () => {
-        let today = new Date();
-        let yesterday = new Date(Date.now() - 86400000);
-        const jobList = await Job.find({ timestamp: { $gte: yesterday, $lt: today } });
-        for (let job of jobList) {
-            let model = {};
-            model.jobName = job.jobName;
-            model.jobType = job.jobType;
-            model.lastBuild = jobinfo.lastBuild.number;
-            let testResult = buildinfo.actions.filter((item) => {
-                return item._class === 'hudson.tasks.junit.TestResultAction'
-            });
-            model.failCount = testResult[0].failCount;
-            model.skipCount = testResult[0].skipCount;
-            model.totalCount = testResult[0].totalCount;
-            model.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
-            model.successRatio = model.successCount / model.totalCount * 100 + '%';
-            model.timestamp = new Date();
-            model.result = buildinfo.result;
-            console.log(123)
-            Job.create(model);
-        }
-    })
-};
-
-dailySaveToJobModel();
+// const dailySaveToJobModel = () => {
+//     schedule.scheduleJob('30 1 1 * * *', async () => {
+//         let today = new Date();
+//         let yesterday = new Date(Date.now() - 86400000);
+//         const jobList = await Job.find({ timestamp: { $gte: yesterday, $lt: today } });
+//         for (let job of jobList) {
+//             let model = {};
+//             model.jobName = job.jobName;
+//             model.jobType = job.jobType;
+//             model.lastBuild = jobinfo.lastBuild.number;
+//             let testResult = buildinfo.actions.filter((item) => {
+//                 return item._class === 'hudson.tasks.junit.TestResultAction'
+//             });
+//             model.failCount = testResult[0].failCount;
+//             model.skipCount = testResult[0].skipCount;
+//             model.totalCount = testResult[0].totalCount;
+//             model.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
+//             model.successRatio = model.successCount / model.totalCount * 100 + '%';
+//             model.timestamp = new Date();
+//             model.result = buildinfo.result;
+//             Job.create(model);
+//         }
+//     })
+// };
+//
+// dailySaveToJobModel();
 
 
 module.exports = router;
