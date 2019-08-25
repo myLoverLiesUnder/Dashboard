23adaa5a77d9f0394e8c962c13b21a2b0218577e
Gzx <1003181892@qq.com>
2019-08-25 14:02:26 +0900

    aaa

--- server/routes/job.js
+++ server/routes/job.js
@@ -24,16 +24,84 @@ router.get('/', async (req, res) => {
             job.jobName = jobItem.jobName;
             job.jobType = jobItem.jobType;
             job.lastBuild = jobinfo.lastBuild.number;
-            let testResult = buildinfo.actions.filter((item) => {
-                return item._class === 'hudson.tasks.junit.TestResultAction'
-            });
-            job.failCount = testResult[0].failCount;
-            job.skipCount = testResult[0].skipCount;
-            job.totalCount = testResult[0].totalCount;
-            job.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
-            job.successRatio = job.successCount / job.totalCount * 100 + '%';
-            job.timestamp = buildinfo.timestamp;
-            job.result = buildinfo.result;
+            job.lastCompletedBuild = jobinfo.lastCompletedBuild.number;
+            //if job is running now
+            if (job.lastBuild === job.lastCompletedBuild) {
+                let testResult = buildinfo.actions.filter((item) => {
+                    return item._class === 'hudson.tasks.junit.TestResultAction'
+                });
+                if(testResult.length > 0){
+                    if (new Date(buildinfo.timestamp).toLocaleDateString() === new Date().toLocaleDateString()) {
+                        job.failCount = testResult[0].failCount;
+                        job.skipCount = testResult[0].skipCount;
+                        job.totalCount = testResult[0].totalCount;
+                        job.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
+                        job.successRatio = job.successCount / job.totalCount * 100 + '%';
+                        job.timestamp = buildinfo.timestamp;
+                        job.result = buildinfo.result;
+                    } else {
+                        job.failCount = testResult[0].totalCount;
+                        job.totalCount = testResult[0].totalCount;
+                        job.successCount = 0;
+                        job.skipCount = 0;
+                        job.successRatio = '0%';
+                        job.result = 'FAIL';
+                    }
+                }else {
+                    job.totalCount = jobinfo.lastSuccessfulBuild.number;
+                    job.successCount = 0;
+                    job.skipCount = 0;
+                    job.failCount = job.totalCount;
+                    job.successRatio = '0%';
+                    job.result = 'FAIL';
+                }
+                // if (buildinfo.result === 'SUCCESS') {
+                //     //if job is running today
+                //     if (new Date(buildinfo.timestamp).toLocaleDateString() === new Date().toLocaleDateString()) {
+                //         job.failCount = testResult[0].failCount;
+                //         job.skipCount = testResult[0].skipCount;
+                //         job.totalCount = testResult[0].totalCount;
+                //         job.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
+                //         job.successRatio = job.successCount / job.totalCount * 100 + '%';
+                //         job.timestamp = buildinfo.timestamp;
+                //         job.result = buildinfo.result;
+                //     } else {
+                //         job.failCount = testResult[0].totalCount;
+                //         job.totalCount = testResult[0].totalCount;
+                //         job.skipCount = 0;
+                //         job.successCount = 0;
+                //         job.successRatio = '0%';
+                //         job.result = 'FAIL';
+                //     }
+                // } else {
+                //     if (testResult.length > 0) {
+                //         if (new Date(buildinfo.timestamp).toLocaleDateString() === new Date().toLocaleDateString()) {
+                //             job.failCount = testResult[0].failCount;
+                //             job.skipCount = testResult[0].skipCount;
+                //             job.totalCount = testResult[0].totalCount;
+                //             job.successCount = testResult[0].totalCount - testResult[0].skipCount - testResult[0].failCount;
+                //             job.successRatio = job.successCount / job.totalCount * 100 + '%';
+                //             job.timestamp = buildinfo.timestamp;
+                //             job.result = buildinfo.result;
+                //         } else {
+                //             job.failCount = testResult[0].totalCount;
+                //             job.totalCount = testResult[0].totalCount;
+                //             job.successCount = 0;
+                //             job.skipCount = 0;
+                //             job.successRatio = '0%';
+                //             job.result = 'FAIL';
+                //         }
+                //     } else {
+                //         job.totalCount = jobinfo.lastSuccessfulBuild.number;
+                //         job.successCount = 0;
+                //         job.skipCount = 0;
+                //         job.failCount = job.totalCount;
+                //         job.successRatio = '0%';
+                //         job.result = 'FAIL';
+                //     }
+                //
+                // }
+            }
             if (job.result !== 'SUCCESS') {
                 temp.status = 'FAIL'
             }
